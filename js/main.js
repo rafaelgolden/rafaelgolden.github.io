598e5a6e300b00fab4d1b0a73967d85a64168845
Rafael Placido <50072974+rafaelgolden@users.noreply.github.com>
2025-05-24 05:57:17 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -61,6 +61,28 @@
         smartSpeed: 1000,
         margin: 24,
         dots: false,
+        loop: false,
+        nav: true,
+        navText: [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            992: {
+                items: 2
+            }
+        }
+    });
+
+    // Equipos carousel
+    $(".equipos-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 24,
+        dots: false,
         loop: true,
         nav: true,
         navText: [
